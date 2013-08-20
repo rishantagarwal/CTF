571d7cfdfeda2e06339c5e0d5ed9bceb6aafda5e
Rishant Agarwal <bindasrishant@gmail.com>
2013-08-21 01:20:32 +0530

    Revert "Final Files for APOGEE CTF 2013 , BITS PILANI"
    This reverts commit 4baf53d310704bbcd969eefacc93a87579398514.

--- connections.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn=mysql_connect("localhost","root","")or die ("check your server connection.");
-$db=mysql_select_db("ctf",$conn);
-if(!isset($_SESSION['userid'])){header('Location:index.php');}
-?>
